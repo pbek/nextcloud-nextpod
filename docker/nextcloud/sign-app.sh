a16ed79861173fe2bf624c51bdf36a6e0d057c5c
Patrizio Bekerle <patrizio@bekerle.com>
2023-02-28 16:12:52 +0100

    dev: exclude more files on releases

--- docker/nextcloud/sign-app.sh
+++ docker/nextcloud/sign-app.sh
@@ -11,7 +11,16 @@ DEPLOYMENT_FILE=${APP_SOURCE}/${APP_NAME}-nc.tar.gz
 
 rm -rf ${APP_DEST} && \
 mkdir ${APP_DEST} && \
-rsync -a --exclude .git* --exclude .gitlab-ci* --exclude .github --exclude screenshot* --exclude docs --exclude tests --exclude vendor --exclude package.* --exclude Makefile --exclude *.db* --exclude docker --exclude *.phar --exclude *.gz --exclude node_modules --exclude .idea ${APP_SOURCE}/ ${APP_DEST} && \
+rsync -a --exclude .git* --exclude .gitlab-ci* --exclude .github --exclude screenshot* --exclude docs \
+         --exclude tests --exclude playwright --exclude vendor --exclude package.* --exclude Makefile \
+         --exclude *.db* --exclude docker --exclude *.phar --exclude *.gz --exclude node_modules \
+         --exclude .idea --exclude package-lock.json --exclude package.json --exclude composer.* \
+         --exclude=babel.config.js --exclude=.drone.yml --exclude=.eslintignore --exclude=.eslintrc.js \
+         --exclude=.gitattributes --exclude=jest.config.js --exclude=.l10nignore --exclude=mkdocs.yml \
+         --exclude=.php_cs.dist --exclude=.php_cs.cache --exclude=CHANGELOG.md --exclude=README.md \
+         --exclude=src --exclude=.stylelintignore --exclude=stylelint.config.js --exclude=.tx \
+         --exclude=releases --exclude=webpack.*.js --exclude=jsconfig.json \
+         ${APP_SOURCE}/ ${APP_DEST} && \
 su -m -c "./occ integrity:sign-app \
   --privateKey=${CERT_PATH}/${APP_NAME}.key \
   --certificate=${CERT_PATH}/${APP_NAME}.crt --path=${APP_DEST}" www-data && \
