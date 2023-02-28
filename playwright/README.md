5c632aa108cc9ee2815952fb6b59ce2bf3d993ca
Patrizio Bekerle <patrizio@bekerle.com>
2023-02-28 16:09:45 +0100

    ci: add playwright for making screenshots

---
+++ playwright/README.md
@@ -0,0 +1,13 @@
+# Playwright
+
+## Installation / Running
+
+This needs the docker container from the `docker` folder to be running.
+
+```bash
+# Install dependencies
+make install-playwright
+
+# Create screenshots
+make screenshots
+```
---
+++ playwright/tests/screenshots.spec.ts
@@ -0,0 +1,46 @@
+import { test, expect } from '@playwright/test';
+
+test('Create screenshots', async ({ page }) => {
+  await page.goto('http://localhost:8081/index.php/login?redirect_url=/index.php/apps/nextpod/actions');
+
+  // Login
+  await page.getByLabel('Account name or email').fill('admin');
+  // await page.getByLabel('Account name or email').press('Tab');
+  await page.getByLabel('Password').fill('admin');
+  await page.getByLabel('Password').press('Enter');
+
+  //
+  // Take screenshot of "Episodes" page
+  //
+
+  //await page.locator('class=list-item-content__main').hover()
+  //await page.locator('class=list-item-content__actions').hover()
+  //await page.locator('class=list-item-content__actions').click()
+  //await page.getByRole('button', { name: 'Actions for item with title "Leadership Lessons from a Disastrous Arctic Expedition"' }).hover()
+
+  // Wait until page renders
+  await page.waitForTimeout(3000);
+  // Playwright tests run on a default viewport size of 1280x720
+  // Try to aim for the 2nd element
+  await page.mouse.click(1150, 288);
+  // Wait until the context menu opens
+  await page.waitForTimeout(1000);
+
+  //await page.getByRole('link', { name: 'LL Leadership Lessons from a Disastrous Arctic Expedition The Art of Manliness Actions for item with title "Leadership Lessons from a Disastrous Arctic Expedition"' }).hover()
+  //await page.getByRole('button', { name: 'Actions for item with title "Leadership Lessons from a Disastrous Arctic Expedition"' }).click();
+  await page.screenshot({ path: '../img/screenshots/episodes.png', fullPage: true });
+
+  //
+  // Take screenshot of "Podcasts" page
+  //
+  await page.getByRole('link', { name: 'Podcasts' }).click();
+  // Wait until page renders
+  await page.waitForTimeout(3000);
+  // Playwright tests run on a default viewport size of 1280x720
+  // Try to aim for the 2nd element
+  await page.mouse.click(1150, 288);
+  // Wait until the context menu opens
+  await page.waitForTimeout(1000);
+  // await page.getByRole('button', { name: 'Actions for item with title "3D Printing Today"' }).click();
+  await page.screenshot({ path: '../img/screenshots/podcasts.png', fullPage: true });
+});
