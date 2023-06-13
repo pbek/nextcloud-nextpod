c9c155b69227ebcedd12eeee160e96c4e99bdbf1
Patrizio Bekerle <patrizio@bekerle.com>
2023-06-13 16:12:33 +0200

    nix: add missing packages

--- shell.nix
+++ shell.nix
@@ -2,7 +2,10 @@
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
     nativeBuildInputs = [
+      pkgs.gnumake
       pkgs.nodejs
+      pkgs.php82
+      pkgs.php82Packages.composer
     ];
 }
 
